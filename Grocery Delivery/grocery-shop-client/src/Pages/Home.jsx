2b135ab2d07457ddef8341808cc4763a21bcbe26
alaminjim <jimalamin7@gmail.com>
2025-08-14 12:13:06 +0600

    added banner section

---
+++ Grocery Delivery/grocery-shop-client/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import MainBanner from "../Components/MainBanner";
+
+const Home = () => {
+  return (
+    <div className="mt-10">
+      <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+        <MainBanner></MainBanner>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- Grocery Delivery/grocery-shop-client/src/Routes/Routes.jsx
+++ Grocery Delivery/grocery-shop-client/src/Routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layouts from "../Layouts/Layouts";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layouts></Layouts>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
