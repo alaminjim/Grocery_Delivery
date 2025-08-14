e94d74b455e4d6ccb84e01567d422f17dd011abe
alaminjim <jimalamin7@gmail.com>
2025-08-14 11:37:35 +0600

    added navbar section

---
+++ Grocery Delivery/grocery-shop-client/src/Context/AppContext.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+
+  const value = {
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
--- Grocery Delivery/grocery-shop-client/src/main.jsx
+++ Grocery Delivery/grocery-shop-client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import router from "./Routes/Routes.jsx";
+import { RouterProvider } from "react-router-dom";
+import { AppContextProvider } from "./Context/AppContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppContextProvider>
+      <RouterProvider router={router} />
+    </AppContextProvider>
+  </StrictMode>
+);
