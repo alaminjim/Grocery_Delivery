fa970c9f5fce26f1860336d680e4c131ded2d4eb
alaminjim <jimalamin7@gmail.com>
2025-08-19 19:45:52 +0600

    added jwt verified and logout

--- Grocery Delivery/grocery-shop-server/controllers/useControllers.js
+++ Grocery Delivery/grocery-shop-server/controllers/useControllers.js
@@ -84,3 +84,38 @@ export const login = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// check auth
+
+export const isAuth = async (req, res) => {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not Authorized" });
+    }
+    const user = await User.findById(userId).select("-password");
+    return res.json({ success: true, user });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// logout
+
+export const logout = (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({ success: true, message: "Logout" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
--- Grocery Delivery/grocery-shop-server/routes/userRoute.js
+++ Grocery Delivery/grocery-shop-server/routes/userRoute.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { login, register } from "../controllers/useControllers.js";
+import {
+  isAuth,
+  login,
+  logout,
+  register,
+} from "../controllers/useControllers.js";
+import authUser from "../middlewares/authUser.js";
 
 const useRouter = express.Router();
 
 useRouter.post("/register", register);
 useRouter.post("/login", login);
+useRouter.get("/is-auth", authUser, isAuth);
+useRouter.get("/logout", authUser, logout);
 
 export default useRouter;
